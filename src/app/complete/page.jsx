7a221a4898d76b66b82ec74a72b2d6899a108df7
amani387 <negaamanuel387@gmail.com>
2025-04-06 14:00:18 +0300

    Fix deployment

--- src/app/complete/page.jsx
+++
@@ -1,9 +0,0 @@
-import CompleteSignup from '@/components/auth/CompleteSignup';
-
-export default function CompleteSignupPage() {
-  return (
-    <div>
-      <CompleteSignup />
-    </div>
-  );
-}
